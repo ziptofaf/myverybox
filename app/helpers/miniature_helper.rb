e993a5bb0cf10bf3c47520608b6655c2ae57b7ae
Marcin <ziptofaf@codenightly.com>
2025-06-17 03:33:08 +0200

    add a previewer for videos

--- app/helpers/miniature_helper.rb
+++ app/helpers/miniature_helper.rb
@@ -3,8 +3,8 @@ module MiniatureHelper
     blob = file_upload.upload.blob
     if blob.content_type.include?('image')
       image_tag blob.representation(resize_and_pad: [256, 256]).processed.url, class: 'ui small image'
-    else
-      ''
+    elsif blob.content_type.include?('video') && blob.previewable?
+      image_tag blob.preview(resize: "256x256").processed, class: 'ui small image'
     end
   end
 end
